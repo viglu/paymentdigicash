541ed2397328fcf431f70e132340416fa64b8318
Luc Vigato <luc.vigato@gmail.com>
2020-04-22 19:24:06 +0200

    Adf translations for the front web-site in de and fr

---
+++ translations/de.php
@@ -0,0 +1,13 @@
+<?php
+
+global $_MODULE;
+$_MODULE = array();
+$_MODULE['<{paymentdigicash}prestashop>qrcode_b2c7e6ecd99042cbae0da909fce0d971'] = 'Wussten Sie, dass Sie mit der DIGICASH App Ihre Bestellung in Sekundenschnelle mit Ihrem Smartphone bezahlen können? Es ist einfach und sicher!';
+$_MODULE['<{paymentdigicash}prestashop>qrcode_a510390e031f83f289e1eb48a9c7187d'] = 'Scannen Sie den QrCode mit Ihrer DIGICASH App!';
+$_MODULE['<{paymentdigicash}prestashop>qrcode_7036f648941f17521729cad3e46e79e9'] = 'Nachdem Sie den QrCode gescannt haben warten Sie bis die Seite automatisch aktualisiert wird!';
+$_MODULE['<{paymentdigicash}prestashop>qrcode_ab116d52ec2aa15f6cc3c5ff66deb399'] = 'Zahlungdaten';
+$_MODULE['<{paymentdigicash}prestashop>qrcode_b5a7adde1af5c87d7fd797b6245c2a39'] = 'Beschreibung';
+$_MODULE['<{paymentdigicash}prestashop>qrcode_b2f40690858b404ed10e62bdf422c704'] = 'Beitrag';
+$_MODULE['<{paymentdigicash}prestashop>qrcode_247e435d5791e2e26e14a760f626602c'] = 'Zahlen Sie nicht mit Digicash, gehen Sie zu Ihrer Bestellung zurück';
+$_MODULE['<{paymentdigicash}prestashop>payment_infos_d697273fd218dc94681e5ce5d02d219c'] = 'Bezahlen Sie mit Ihrem Smartphone in Sekundenschnelle. Digicash ist nur mit luxemburgischen Bankkonten verfügbar.';
+$_MODULE['<{paymentdigicash}prestashop>validation_e2b7dec8fa4b498156dfee6e4c84b156'] = 'Diese Zahlungsmethode ist nicht verfügbar.';
---
+++ translations/fr.php
@@ -0,0 +1,14 @@
+<?php
+
+global $_MODULE;
+$_MODULE = array();
+$_MODULE['<{paymentdigicash}prestashop>paymentdigicash_2f9ec5f8f5904df152d6d2a7fe41ea56'] = 'Digicash by Payconiq';
+$_MODULE['<{paymentdigicash}prestashop>qrcode_b2c7e6ecd99042cbae0da909fce0d971'] = 'Saviez-vous qu\'avec l\'application DIGICASH, vous pouvez payer votre commande en quelques secondes avec votre smartphone ? C\'est facile et sécurisé !';
+$_MODULE['<{paymentdigicash}prestashop>qrcode_a510390e031f83f289e1eb48a9c7187d'] = 'Scannez le QrCode avec votre application DIGICASH !';
+$_MODULE['<{paymentdigicash}prestashop>qrcode_7036f648941f17521729cad3e46e79e9'] = 'Après avoir scanné le QrCode, attendez que la page se rafraîchisse automatiquement !';
+$_MODULE['<{paymentdigicash}prestashop>qrcode_ab116d52ec2aa15f6cc3c5ff66deb399'] = 'Détails du paiement';
+$_MODULE['<{paymentdigicash}prestashop>qrcode_b5a7adde1af5c87d7fd797b6245c2a39'] = 'Déscription';
+$_MODULE['<{paymentdigicash}prestashop>qrcode_b2f40690858b404ed10e62bdf422c704'] = 'Montant';
+$_MODULE['<{paymentdigicash}prestashop>qrcode_247e435d5791e2e26e14a760f626602c'] = 'Ne payez pas avec Digicash, retournez à votre commande';
+$_MODULE['<{paymentdigicash}prestashop>payment_infos_d697273fd218dc94681e5ce5d02d219c'] = 'Payez avec votre smartphone en quelques secondes. Digicash n\'est disponible qu\'avec les comptes bancaires luxembourgeois.';
+$_MODULE['<{paymentdigicash}prestashop>validation_e2b7dec8fa4b498156dfee6e4c84b156'] = 'Ce mode de paiement n\'est pas disponible.';
